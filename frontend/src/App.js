2ea7c2d17339b6c5f1c567b30c5e2d59f70e543a
alexmerlo0623 <a.merlo@ufl.edu>
2024-11-06 21:53:39 -0500

    fixed recipe page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <IngredientsProvider>
+      <IngredientsProvider>
           <Navbar />
           <div className="pages">
             <Routes>
